b4e74a1424398ee87ae2f84d6d883dc7604be758
Nathan Hui <ntlhui@gmail.com>
2023-02-12 21:47:07 -0800

    Added initial laser parallax model

---
+++ laser_parallax/setup.py
@@ -0,0 +1,26 @@
+"""Laser Parallax Model
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='laser_parallax_model',
+    version='0.0.0.1',
+    author='UCSD Engineers for Exploration',
+    author_email='e4e@eng.ucsd.edu',
+    entry_points={
+    },
+    packages=find_packages(),
+    install_requires=[
+        'jupyter',
+        'matplotlib',
+        'numpy',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'coverage',
+            'pylint',
+            'wheel',
+        ]
+    },
+)
