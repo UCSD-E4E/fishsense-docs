b2e9810e9b1c81f89a32b989e8bfbca745c64712
Falafel72 <kylehu2000@gmail.com>
2023-04-14 14:49:46 -0700

    fixed merge conflicts

--- laser_parallax/laser_parallax.py
+++ laser_parallax/laser_parallax.py
@@ -1,12 +1,49 @@
 import numpy as np
 
 def image_coordinate_to_projected_point(
-        image_point: np.ndarray, pixel_pitch_mm: float, focal_length_mm: float):
+        image_point: np.ndarray, 
+        pixel_pitch_mm: float, 
+        focal_length_mm: float) -> np.ndarray:
     assert isinstance(image_point, np.ndarray)
     # S += sensor_size_px / 2
     I_project = image_point * pixel_pitch_mm / 1e3
     I = np.array([I_project[0], I_project[1], -focal_length_mm / 1e3])
     return I
+
+def image_coordinate_to_projected_point_vec(
+        image_points: np.ndarray, 
+        pixel_pitch_mm: float, 
+        focal_length_mm: float) -> np.ndarray:
+    assert isinstance(image_points, np.ndarray)
+    I_project = image_points * pixel_pitch_mm / 1e3
+    I = np.zeros((I_project.shape[0], I_project.shape[1] + 1))
+    I[...,:-1] = I_project
+    I[...,-1] = -focal_length_mm * 1e-3
+    return I
+
+def compute_world_point_from_depth(
+        camera_params: tuple, 
+        image_coordinate: np.ndarray,
+        depth: float) -> np.ndarray:
+    # Assumes the depth is known, calculates world point based on image point
+    projected_point = image_coordinate_to_projected_point(
+        image_point=image_coordinate,
+        pixel_pitch_mm=camera_params[3],
+        focal_length_mm=camera_params[0]) 
+    v = -projected_point / np.linalg.norm(projected_point)
+    return v * depth/v[2]
+
+def compute_world_points_from_depths(
+        camera_params: tuple, 
+        image_coordinates: np.ndarray,
+        depths: np.ndarray) -> np.ndarray:
+    # Assumes the depth is known, calculates world point based on image point
+    projected_point = image_coordinate_to_projected_point_vec(
+        image_points=image_coordinates,
+        pixel_pitch_mm=camera_params[3],
+        focal_length_mm=camera_params[0]) 
+    v = -projected_point / (np.linalg.norm(projected_point,axis=1)[...,np.newaxis])
+    return v * depths[:,np.newaxis]/v[:,2,np.newaxis]
 
 def compute_world_point(
         laser_origin: np.ndarray,
